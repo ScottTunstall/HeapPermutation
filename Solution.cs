5359496ca4e287c9e6dd84d858f1d0b1980226d2
Scott Tunstall <scott.tunstall@ntlworld.com>
2022-02-12 14:26:16 +0000

    Tidied up code a little.

--- Solution.cs
+++ Solution.cs
@@ -8,35 +8,35 @@ namespace Permutations
 {
     public class Solution
     {
-        public void HeapPermutation<T>(T[] a, int size)
+        public void HeapPermutation<T>(T[] arr, int size)
         {
             // if size becomes 1 then print the obtained
             // permutation
             if (size == 1)
             {
-                for (int j = 0; j < a.Length; j++)
+                for (int j = 0; j < arr.Length; j++)
                 {
-                    Console.Write(a[j]);
+                    Console.Write(arr[j]);
                 }
                 Console.WriteLine();
             }
 
             for (int i = 0; i < size; i++)
             {
-                HeapPermutation(a, size - 1);
+                HeapPermutation(arr, size - 1);
 
                 // if size is odd, swap 0th i.e (first) and
                 // (size-1)th i.e (last) element
                 if ((size & 1) == 1)
                 {
-                    Swap(ref a[0], ref a[size - 1]);
+                    Swap(ref arr[0], ref arr[size - 1]);
                 }
 
                 // If size is even, swap ith and
                 // (size-1)th i.e (last) element
                 else
                 {
-                    Swap(ref a[i], ref a[size - 1]);
+                    Swap(ref arr[i], ref arr[size - 1]);
                 }
             }
         }
